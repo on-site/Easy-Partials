cff4573df29f8da703f8f0ab65038ec840367540
Samer Abukhait <abukhait@gmail.com>
2010-07-15 16:32:56 -0700

    cleanup

--- easy_partials.gemspec
+++ easy_partials.gemspec
@@ -5,11 +5,11 @@
 
 Gem::Specification.new do |s|
   s.name = %q{easy_partials}
-  s.version = "0.0.1"
+  s.version = "0.1.0"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Samer Abukhait"]
-  s.date = %q{2010-07-15}
+  s.date = %q{2010-07-16}
   s.description = %q{An easier way to call partials}
   s.email = %q{abukhait@gmail.com}
   s.extra_rdoc_files = [
--- lib/easy_partials.rb
+++ lib/easy_partials.rb
@@ -1,9 +1,49 @@
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+class Object
+  # Obtain the metaclass of this object instance.
+  def metaclass
+    class << self
+      self
+    end
+  end
 
-module Easypartials
+  # Define an instance method using a symbol for the method name and a
+  # block for the method contents.  This makes it so a closure can be
+  # used to define a singleton method.
+  def meta_def(name, &block)
+    metaclass.send :define_method, name, &block
+  end
+
+  # Define an instance method using a block that will accept a block
+  # as the first parameter, and the rest of the arguments as the
+  # second.  This requires the block to have the first parameter act
+  # as the block.
+  #
+  # For example:
+  # o.meta_def_with_block(:say_hi) { |block, name|
+  #   block.call
+  #   puts "hello #{name}!"
+  # }
+  #
+  # Will create a method that can be invoked as such:
+  # o.say_hi("Mike") { puts "Saying hi:" }
+  #
+  # Implicitly, 2 methods are created... __real__say_hi, which is
+  # created using the given block, and say_hi, which will accept the
+  # arguments and block and pass them to __real__say_hi in reverse
+  # order.
+  def meta_def_with_block(name, &block)
+    meta_def "__real__#{name}".to_sym, &block
+    metaclass.instance_eval "
+      def #{name}(*args, &block)
+        __real__#{name} block, *args
+      end
+    "
+  end
+end
+
+module EasyPartials
   def self.shared_directories
-    @ep_shared_directories || []
+    @ep_shared_directories || ["shared"]
   end
 
   def self.shared_directories=(arglist)
@@ -12,64 +52,112 @@ module Easypartials
 end
 
 module ApplicationHelper
-  alias_method :method_missing_without_easy_partials, :method_missing
-  alias_method :respond_to_without_easy_partials, :respond_to?
 
-  METHOD_REGEXP = /^_(.+)$/
+  METHOD_REGEXP = /^_/
 
-  def respond_to_with_easy_partials(method_name, inc_priv = false)
+  alias_method :original_respond_to?, :respond_to?
+
+  def respond_to?(method_name, inc_priv = false)
     return true if method_name =~ METHOD_REGEXP
 
-    respond_to_without_easy_partials(method_name, inc_priv)
+    original_respond_to?(method_name, inc_priv)
   end
 
-  def method_missing_with_easy_partials(method_name, *args, &block)
+  alias_method :original_method_missing, :method_missing
+
+  def method_missing(method_name, *args, &block)
     method_str = method_name.to_s
 
-    if method_str =~ METHOD_REGEXP
-      partial_name = method_str[METHOD_REGEXP, 1]
-
-      self.class.class_eval <<ENDEVAL
-def #{method_str}(*args, &block)
-  begin
-    concat_partial "#{partial_name}", *args, &block
-  rescue ActionView::MissingTemplate
-    last_exc = nil
-    done = false
-    dirs = Easypartials.shared_directories
-    dirs.each do |shared|
-      begin
-        concat_partial shared + "/#{partial_name}", *args, &block
-        done = true
-      rescue ActionView::MissingTemplate => exc
-        last_exc = exc
-      end
-      break if done
-    end
-    unless done
-      exc = ActionView::MissingTemplate.new dirs, "#{partial_name}"
-      raise exc
-    end
-  end
-end
-ENDEVAL
-      self.send(method_str, *args, &block)
+    if method_str.sub! METHOD_REGEXP, ''
+      locations = [method_str]
+      locations.push *additional_partials(method_str)
+      new_method = partial_method locations, *args, &block
+      meta_def_with_block method_name, &new_method
     else
-      method_missing_without_easy_partials method_name, *args, &block
+      original_method_missing method_name, *args, &block
     end
   end
 
-  alias_method :method_missing, :method_missing_with_easy_partials
-  alias_method :respond_to?, :respond_to_with_easy_partials
+  def additional_partials(partial_name)
+    @additional_partials ||= EasyPartials.shared_directories
+    @additional_partials.map { |location| "#{location}/#{partial_name}" }
+  end
 
-  # Concat the given partial.
-  def concat_partial(partial_name, locals = {}, &block)
-    unless block.nil?
-      locals.merge! :body => capture(&block)
+  # Utility method to create and invoke a Proc which will concat the
+  # partial given the possible locations.  The Proc is then returned
+  # so it can be added as a new method for caching purposes (otherwise
+  # method_missing will have to be invoked each time the partial is
+  # invoked).  The locations parameter is modified in the process.
+  # This is used by method_missing.
+  def partial_method(locations, *args, &block)
+    raise "No possible locations!" if locations.empty?
+    partial_name = locations.delete_at 0
+    new_method = lambda do |block, *args|
+      if params[:format] == "pdf"
+        invoke_partial partial_name, *args, &block
+      else
+        concat_partial partial_name, *args, &block
+      end
+    end
+    begin
+      new_method.call block, *args
+    rescue ActionView::MissingTemplate
+      if locations.empty?
+        raise
+      else
+        new_method = partial_method locations, *args, &block
+      end
     end
 
-    content = render :partial => partial_name, :locals => locals
-    concat content
-    nil
+    new_method
   end
+
+  def invoke_partial(partial, *args, &block)
+    locals = {}
+
+    if args.length == 1 && args[0].is_a?(Hash)
+      locals.merge! args[0]
+    else
+      locals.merge! :args => args
+    end
+
+    locals.merge! :body => capture(&block) if block
+    locals[:body] = nil unless locals[:body]
+
+    if locals.has_key? :collection
+      return "" if locals[:collection].blank?
+      render :partial => partial.to_s, :collection => locals[:collection],
+             :locals => locals.except(:collection)
+    else
+      render :partial => partial.to_s, :locals => locals
+    end
+
+  end
+
+  # Used to create nice templated "tags" while keeping the html out of
+  # our helpers.  Additionally, this can be invoked implicitly by
+  # invoking the partial as a method with "_" prepended to the name.
+  #
+  # Invoking the method:
+  #
+  #   <% concat_partial "my_partial", { :var => "value" } do %>
+  #     <strong>Contents stored as a "body" local</strong>
+  #   <% end %>
+  #
+  # Or invoking implicitly:
+  #
+  #   <% _my_partial :var => "value" do %>
+  #     <strong>Contents stored as a "body" local</strong>
+  #   <% end %>
+  #
+  # Note that with the implicit partials the partial will first be
+  # searched for locally within the current view directory, and then
+  # additional directories defined by the controller level
+  # 'additional_partials' method, and finally within the views/shared
+  # directory.
+  def concat_partial(partial, *args, &block)
+    rendered = invoke_partial partial, *args, &block
+    concat rendered
+  end
+
 end
